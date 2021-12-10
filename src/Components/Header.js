6709c1eed09edc7e28b5a9122f405662befac5cc
sarra <sarrah.aouina@gmail.com>
2021-12-10 12:49:17 +0100

    checkpointreactjs

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import gomy from '../image/gomy.png'
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src={gomy} alt="Gomycode" height="100"  />  <h1>Checkpoint React Js</h1>
+        </header>
+        );
+    };
+    export default Header;
---
+++ src/Components/Navigation.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const Navigation = () => {
+    return (
+        <nav className='navbar'>
+            <ul>
+              
+                    <a href="#">Home    </a>
+                
+                    <a href="#">My Books    </a>
+                
+                    <a href="#">Favorites</a>
+                
+            </ul>
+        </nav>
+    );
+};
+export default Navigation;
